1651d43f301c747f5a1050cf685cc380d06412f1
Casprine <casprine.001@gmail.com>
2018-09-28 22:07:36 +0000

    minor update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Atlas',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'atlas-starter',
+        short_name: 'atlas',
         start_url: '/',
-        background_color: '#663399',
+        background_color: '#1e2a78',
         theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
